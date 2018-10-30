fe057553bbd826b7a98337f7aa6ddb3e28057f9c
Stuart Terrett <stuart.terrett@oracle.com>
2018-10-29 20:37:08 -0400

    Streams pt 1

---
+++ java_tutorial/src/streams/Main.java
@@ -0,0 +1,41 @@
+package streams;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+    private static final String[] WORDS = {
+            "snow",
+            "eggnog",
+            "friendly",
+            "future",
+            "self",
+            "spy",
+            "questionable",
+            "dead",
+            "spade",
+            "perpetual",
+            "faulty",
+            "subsequent"
+    };
+
+    public static void main(String[] args) {
+        Stream.of(WORDS).forEach(w -> System.out.format("  %s%n", w));
+        Stream.of(WORDS).forEach(System.out::println);
+        System.out.println(Stream.of(WORDS).map(w -> w + "!").collect(toList()));
+        System.out.println(Stream.of(WORDS).map(w -> w.replace("i", "eye")).collect(toList()));
+        System.out.println(Stream.of(WORDS).map(String::toUpperCase).collect(toList()));
+        System.out.println(Stream.of(WORDS).filter(w -> w.length() < 4).collect(toList()));
+        System.out.println(Stream.of(WORDS).filter(w -> w.contains("b")).collect(toList()));
+        System.out.println(Stream.of(WORDS).filter(w -> w.length() % 2 == 0).collect(toList()));
+
+        Stream.of(WORDS)
+            .map(String::toUpperCase)
+            .filter(w -> w.contains("E"))
+            .forEach(System.out::println);
+
+        System.out.println(Stream.of(WORDS).toArray(String[]::new));
+    }
+}
